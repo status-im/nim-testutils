87059cfcf5733c30fcc243f8956a58d070ebdd92
ringabout <43030857+ringabout@users.noreply.github.com>
2022-11-24 13:30:09 +0800

    use named block and break pairs

--- testutils/spec.nim
+++ testutils/spec.nim
@@ -151,13 +151,13 @@ proc parseTestFile*(config: TestConfig; filePath: string): TestSpec =
   result.path = absolutePath(filePath)
   result.pathComponents = splitFile result.path
   result.config = config
-  block:
+  block label:
     var
       f = newFileStream(result.path, fmRead)
     if f == nil:
       # XXX crash?
       echo "Parsing error: cannot open " & result.path
-      break
+      break label
 
     var
       outputSection = false
